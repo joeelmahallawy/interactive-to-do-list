bca2aa0d092c4b8d344ce520dd667d9bb6342b60
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2021-08-13 13:45:16 -0400

    added update method for task list

--- components/TasksContainer.tsx
+++ components/TasksContainer.tsx
@@ -1,29 +1,44 @@
 import { Box, Button, Flex, Heading, Input } from "@chakra-ui/react";
 import React, { useRef, useState } from "react";
-import { useMethods } from "react-use";
+import { useMethods, useUpdate } from "react-use";
 import createMethods, { initState } from "../states/useMethods";
-import RenderToDo from "./renderToDo";
+import RenderToDo from "./RenderToDo";
 
 export default function TasksContainer() {
   const [state, methods] = useMethods(createMethods, initState);
+  const task = useRef();
+  const updateSelf = useUpdate();
 
   return (
     <>
       <Box mt={5}>
         <Flex>
-          <Input variant="filled" placeholder="Filled" size="lg" w="50%" />
-          <Button size="lg" colorScheme="linkedin" ml={3}>
+          <Input
+            variant="filled"
+            placeholder="Filled"
+            size="lg"
+            w="50%"
+            ref={task}
+          />
+          <Button
+            size="lg"
+            colorScheme="linkedin"
+            ml={3}
+            onClick={() => {
+              // @ts-expect-error
+              methods.addToDo(task.current.value);
+              updateSelf();
+            }}
+          >
             Add
           </Button>
         </Flex>
-        <Box mt={5} p={1}>
-          <Heading border="5px solid red" p={3}>
+        <Box mt={5} border="0.5px solid lightgray">
+          <Heading p={3} borderBottom="0.5px solid lightgray">
             To do List
           </Heading>
+          <RenderToDo />
         </Box>
-        yo
-        <RenderToDo />
-        {console.log(state.todo)}
       </Box>
     </>
   );
--- components/renderToDo.tsx
+++ components/renderToDo.tsx
@@ -1,9 +1,27 @@
-import { Heading } from "@chakra-ui/react";
-import React from "react";
+import { Box, Heading, Text, Flex } from "@chakra-ui/react";
+
 import createMethods, { initState } from "../states/useMethods";
 import { useMethods } from "react-use";
-const [state, methods] = useMethods(createMethods, initState);
+import React from "react";
 
-export default function RenderToDo() {
-  return <Heading>say word</Heading>;
+export default function RenderToDo(arr) {
+  const [state, methods] = useMethods(createMethods, initState);
+  //   console.log(state.todo);
+
+  return state.todo.map((task, i) => {
+    return (
+      <Flex
+        borderBottom="0.5px solid lightgray"
+        key={i}
+        alignContent="center"
+        mt={3}
+        justifyContent="space-between"
+        p={3}
+      >
+        <Text>{task}</Text>
+        <Box>TIME HERE</Box>
+      </Flex>
+    );
+  });
+  //   return <Heading>YOOO</Heading>;
 }
